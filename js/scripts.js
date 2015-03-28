2c44a008f34b831516cdbd99bf45eb755f69d99d
Martin Green <m.r.green@hotmail.co.uk>
2015-03-28 14:49:11 +0000

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,328 @@
+/********** WEBSITE PRELOADER JS **********/
+
+(function($) {
+	var items = new Array(),
+		errors = new Array(),
+		onComplete = function() {},
+		current = 0;
+	
+	var jpreOptions = {
+		splashVPos: '50%',
+		loaderVPos: '50%',
+		splashID: '#jpreContent',
+		showSplash: false,
+		showPercentage: true,
+		autoClose: true,
+		closeBtnText: 'Start!',
+		onetimeLoad: false,
+		debugMode: false,
+		splashFunction: function() {}
+	}
+	
+	//cookie
+	var getCookie = function() {
+		if( jpreOptions.onetimeLoad ) {
+			var cookies = document.cookie.split('; ');
+			for (var i = 0, parts; (parts = cookies[i] && cookies[i].split('=')); i++) {
+				if ((parts.shift()) === "jpreLoader") {
+					return (parts.join('='));
+				}
+			}
+			return false;
+		} else {
+			return false;
+		}
+		
+	}
+	var setCookie = function(expires) {
+		if( jpreOptions.onetimeLoad ) {
+			var exdate = new Date();
+			exdate.setDate( exdate.getDate() + expires );
+			var c_value = ((expires==null) ? "" : "expires=" + exdate.toUTCString());
+			document.cookie="jpreLoader=loaded; " + c_value;
+		}
+	}
+	
+	//create jpreLoader UI
+	var createContainer = function() {
+		
+		jOverlay = $('<div></div>')
+		.attr('id', 'jpreOverlay')
+		.css({
+			position: "fixed",
+			top: 0,
+			left: 0,
+			width: '100%',
+			height: '100%',
+			zIndex: 9999999
+		})
+		.appendTo('body');
+		
+		jLoader = $('<div></div>')
+		.attr('id', 'jpreLoader')
+		.appendTo(jOverlay);
+		
+		var posWidth = $(window).width() - $(jLoader).width();
+		$(jLoader).css({
+			position: 'absolute',
+			top: '50%',
+			left: '50%'
+		});
+		
+		jBar = $('<div></div>')
+		.attr('id', 'jpreBar')
+		.css({
+			width: '0%',
+			height: '100%'
+		})
+		.appendTo(jLoader);
+		
+		if(jpreOptions.showPercentage) {
+			jPer = $('<div></div>')
+			.attr('id', 'jprePercentage')
+			.css({
+				position: 'relative',
+				height: '100%'
+			})
+			.appendTo(jLoader)
+			.html('00%');
+		}
+		if( !jpreOptions.autoclose ) {
+			jButton = $('<div></div>')
+			.attr('id', 'jpreButton')
+			.on('click', function() {
+				loadComplete();
+			})
+			.css({
+				position: 'relative',
+				height: '100%'
+			})
+			.appendTo(jLoader)
+			.text(jpreOptions.closeBtnText)
+			.hide();
+		}
+	}
+	
+	//get all images from css and <img> tag
+	var getImages = function(element) {
+		$(element).find('*:not(script)').each(function() {
+			var url = "";
+
+			if ($(this).css('background-image').indexOf('none') == -1 && $(this).css('background-image').indexOf('-gradient') == -1) {
+				url = $(this).css('background-image');
+				if(url.indexOf('url') != -1) {
+					var temp = url.match(/url\((.*?)\)/);
+					url = temp[1].replace(/\"/g, '');
+				}
+			} else if ($(this).get(0).nodeName.toLowerCase() == 'img' && typeof($(this).attr('src')) != 'undefined') {
+				url = $(this).attr('src');
+			}
+			
+			if (url.length > 0) {
+				items.push(url);
+			}
+		});
+	}
+	
+	//create preloaded image
+	var preloading = function() {
+		for (var i = 0; i < items.length; i++) {
+			if(loadImg(items[i]));
+		}
+	}
+	var loadImg = function(url) {
+		var imgLoad = new Image();
+		$(imgLoad)
+		.load(function() {
+			completeLoading();
+		})
+		.error(function() {
+			errors.push($(this).attr('src'));
+			completeLoading();
+		})
+		.attr('src', url);
+	}
+	
+	//update progress bar once image loaded
+	var completeLoading = function() {
+		current++;
+
+		var per = Math.round((current / items.length) * 100);
+		$(jBar).stop().animate({
+			width: per + '%'
+		}, 500, 'linear');
+		
+		if(jpreOptions.showPercentage) {
+			$(jPer).text(per+"%");
+		}
+		
+		//if all images loaded
+		if(current >= items.length) {
+			current = items.length;
+			setCookie();	//create cookie
+			
+			if(jpreOptions.showPercentage) {
+				$(jPer).text("100%");
+			}
+			
+			//fire debug mode
+			if (jpreOptions.debugMode) {
+				var error = debug();
+			}
+			
+			
+			//max progress bar
+			$(jBar).stop().animate({
+				width: '100%'
+			}, 500, 'linear', function() {
+				//autoclose on
+				if( jpreOptions.autoClose )
+					loadComplete();
+				else
+					$(jButton).fadeIn(1000);
+			});	
+		}	
+	}
+	
+	//triggered when all images are loaded
+	var loadComplete = function() {
+		$(jOverlay).fadeOut(800, function() {
+			$(jOverlay).remove();
+			onComplete();	//callback function
+		});
+	}
+	
+	//debug mode
+	var debug = function() {
+		if(errors.length > 0) {
+			var str = 'ERROR - IMAGE FILES MISSING!!!\n\r'
+			str	+= errors.length + ' image files cound not be found. \n\r';	
+			str += 'Please check your image paths and filenames:\n\r';
+			for (var i = 0; i < errors.length; i++) {
+				str += '- ' + errors[i] + '\n\r';
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	$.fn.jpreLoader = function(options, callback) {
+        if(options) {
+            $.extend(jpreOptions, options );
+        }
+		if(typeof callback == 'function') {
+			onComplete = callback;
+		}
+		
+		//show preloader once JS loaded
+		$('body').css({
+			'display': 'block'
+		});
+		
+		return this.each(function() {
+			if( !(getCookie()) ) {
+				createContainer();
+				getImages(this);
+				preloading();
+			}
+			else {	//onetime load / cookie is set
+				$(jpreOptions.splashID).remove();
+				onComplete();
+			}
+		});
+    };
+
+})(jQuery);
+
+/********** WAYPOINTS JS **********/
+
+$(document).ready(function() {
+
+	$('.wp1').waypoint(function() {
+		$('.wp1').addClass('animated fadeInDown');
+	}, {
+		offset: '75%'
+	});
+	$('.wp2').waypoint(function() {
+		$('.wp2').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp3').waypoint(function() {
+		$('.wp3').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp4').waypoint(function() {
+		$('.wp4').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp5').waypoint(function() {
+		$('.wp5').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp6').waypoint(function() {
+		$('.wp6').addClass('animated fadeInDown');
+	}, {
+		offset: '100%'
+	});
+	$('.wp7').waypoint(function() {
+		$('.wp7').addClass('animated fadeInDown');
+	}, {
+		offset: '100%'
+	});
+});
+
+/********** ARROW ANIMATED SCROLL JS **********/
+  
+animUp();
+   
+function animUp() {
+    $(".welcome_header_arrow").animate({ bottom: "40px" }, "slow", "swing", animDown);
+}
+
+function animDown() {
+    $(".welcome_header_arrow").animate({ bottom: "25px" }, "slow", "swing", animUp);
+}
+  
+    $(".welcome_header_arrow").click(function(){
+
+   	$("html, body").animate({scrollTop:$(window).height()}, 2500, "swing");
+
+});
+
+/********** HEAD FIXED AND VISIBLE JS **********/
+
+$(function() {
+		// grab the initial top offset of the navigation 
+		var sticky_navigation_offset_top = $('#main-header').offset().top;
+		
+		// our function that decides weather the navigation bar should have "fixed" css position or not.
+		var sticky_navigation = function(){
+			var scroll_top = $(window).scrollTop(); // our current vertical position from the top
+			
+		// if we've scrolled more than the navigation, change its position to fixed to stick to top, otherwise change it back to relative
+			if (scroll_top > sticky_navigation_offset_top) { 
+				$('#main-header').addClass('fixed');
+			} else {
+				$('#main-header').removeClass('fixed');
+			}   
+		};
+		
+		// run our function on load
+		sticky_navigation();
+		
+		// and run it again every time you scroll
+		$(window).scroll(function() {
+			 sticky_navigation();
+		});
+		
+		// NOT required:
+		// for this demo disable all links that point to "#"
+		$('a[href="#"]').click(function(event){ 
+			event.preventDefault(); 
+		});
+});
